fd84bab6970a56b53ec36881ffb14586ec1793c5
Freddy <Freddy@PC_LIMA>
2020-05-05 10:01:36 -0500

    Se ha cambiado el nombre del proyecto

--- LP2Soft/src/javaapplication13/NewClass.java
+++ LP2Soft/src/javaapplication13/NewClass.java
@@ -9,13 +9,6 @@ package javaapplication13;
  *
  * @author Freddy
  */
-public class JavaApplication13 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class NewClass {
     
 }
